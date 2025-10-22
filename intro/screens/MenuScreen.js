28184464db34d8bb27c065200ab4551b31fb39d5
Patito3000 <124050154@upq.edu.mx>
2025-10-21 23:40:52 -0600

    Textinput
    Agregando la practica de textinput

--- intro/screens/MenuScreen.js
+++ intro/screens/MenuScreen.js
@@ -1,8 +1,11 @@
-import { Text, StyleSheet, View, Button } from 'react-native'
+import { Text, StyleSheet, View, Button, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import ContadorScreen from './ContadorScreen'
 import BotonesScreen from './BotonesScreen'
 import ListScreen from './ListScreen'
+import TextInputScreen from './TextInputScreen'
+
+//Pressable es otra forma de hacer botones
 
 export default function MenuScreen() {
     const [screen, setScreen]= useState('menu');
@@ -16,6 +19,10 @@ export default function MenuScreen() {
             return <ListScreen/>      
         case 'Section':
             return <SectionListScreen/>
+        case 'Text':
+            return <TextInputScreen/>
+        
+        
         case 'menu':
             default:
                  return (
@@ -25,11 +32,15 @@ export default function MenuScreen() {
                         <Button color='yellow' title='Pract:Buttons' onPress={()=>setScreen('botones')} />
                         <Button color='blue' title='Pract:List' onPress={()=>setScreen('List')} />
                         <Button color='green' title='Pract:Section' onPress={()=>setScreen('Section')} /> 
-                        <Button color='yellow' title='Pract:List' onPress={()=>setScreen('contador')} />
+                        <Button color='yellow' title='Pract:Text' onPress={()=>setScreen('Text')} />
                         <Button color='pink' title='Pract:Modal' onPress={()=>setScreen('botones')} />
                         <Button color='black' title='Pract:Scroll' onPress={()=>setScreen('contador')} />
                         <Button color='red' title='Pract:Sheet' onPress={()=>setScreen('botones')} />
-                        <Button color='brown' title='Pract:Text' onPress={()=>setScreen('botones')} />    
+                        <Button color='brown' title='Pract:Text' onPress={()=>setScreen('botones')} />   
+                         
+                        <Pressable>
+                            <Text>+1</Text>
+                        </Pressable>
                     </View>
                 )
 
